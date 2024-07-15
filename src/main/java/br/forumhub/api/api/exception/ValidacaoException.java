62b8c9d38cfd5dd91036f7697c3f6de219df5a4a
Leandro Campos <camposweb@gmail.com>
2024-07-15 02:08:53 -0300

    feat: Criando exceções e tratando os erros

---
+++ src/main/java/br/forumhub/api/api/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package br.forumhub.api.api.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
