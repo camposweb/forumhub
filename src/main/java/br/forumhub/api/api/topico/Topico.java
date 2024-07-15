9c636342a15c3483c77c53a4e61673cda7495fb7
Leandro Campos <camposweb@gmail.com>
2024-07-15 01:12:32 -0300

    feat: Criação entidade Topico

--- src/main/java/br/forumhub/api/api/topico/Topico.java
+++ src/main/java/br/forumhub/api/api/topico/Topico.java
@@ -20,8 +20,10 @@ public class Topico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String titulo;
 
+    @Column(unique = true)
     private String mensagem;
 
     @Column(name = "data_criacao")
